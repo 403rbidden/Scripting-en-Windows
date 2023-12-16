823683cf0d2c2f005a724ec33bde743c103b1acd
MJ <84123624+403rbidden@users.noreply.github.com>
2023-12-16 11:00:22 +0100

    Update renamer.ps1

--- PowerShell/Renamer/renamer.ps1
+++ PowerShell/Renamer/renamer.ps1
@@ -1,15 +1,24 @@
-#renamer
+# Renamer
 
+# ⚠️
+# El programa está aún en fase de desarrollo, se recomienda mucha precaución.
+# ¡Realizar las pruebas únicamente en un entorno controlado!
+
+# Establece la codificación de salida como UTF-8
 $OutputEncoding = [System.Text.Encoding]::UTF8
 
-#$rutaCarpeta = Read-Host "Ingresa la ruta de la carpeta que deseas analizar"
-$rutaCarpeta = "C:\Users\mjoca\Downloads\jdownloader_descargas"
+# Define la ruta de la carpeta a analizar
+#$rutaCarpeta = "C:\Users\mjoca\Downloads\jdownloader_descargas"
+$rutaCarpeta = Read-Host "Ingresa la ruta de la carpeta que deseas analizar"
 
+# Obtiene el contenido de la carpeta
 $contenidoCarpeta = Get-ChildItem -Path $rutaCarpeta
 $contenidoCarpeta
 
+# Inicializa el contador de intentos
 $intentos = 0
 
+# Función para validar la opción de analizar todos los archivos o seleccionar un tipo específico
 function ValidarOpcion {
     param (
         [string]$opcion
@@ -18,6 +27,7 @@ function ValidarOpcion {
         $extension = "*"
     }
     elseif ($opcion -eq "n") {
+        # Solicita al usuario seleccionar el tipo de archivo
         Write-Host "`nSelecciona el tipo de archivo que deseas analizar:"
         Write-Host "A. Documento de texto (TXT)"
         Write-Host "B. Presentacion (PPTX)"
@@ -27,6 +37,8 @@ function ValidarOpcion {
         Write-Host "F. Documento de Word (DOCX)"
         Write-Host "G. Documento de Excel (XLSX)"
         Write-Host "H. Otros archivos"
+        
+        # Utiliza un switch para asignar la extensión según la opción seleccionada
         $extension = switch (Read-Host "`nIngresa la opcion correspondiente") {
             "a" { "txt" }
             "b" { "pptx" }
@@ -49,17 +61,20 @@ function ValidarOpcion {
     return $extension
 }
 
+# Función para listar los archivos con una extensión específica
 function ListarArchivos {
     param (
         [string]$ruta,
         [string]$extension
     )
     Write-Host "`nListado de los archivos con la extension '.$extension' especificada:"
+    # Obtiene y muestra los archivos con la extensión especificada
     Get-ChildItem -Path $ruta -Filter "*.$extension" | ForEach-Object {
         Write-Host ("- {0}" -f $_.Name)
     }
 }
 
+# Función para cambiar el nombre de archivos eliminando un texto específico
 function CambiarNombreArchivos {
     param (
         [string]$ruta,
@@ -68,6 +83,7 @@ function CambiarNombreArchivos {
     )
     $contadorArchivos = 0
     Write-Host ("`nArchivos renombrados:`n")
+    # Obtiene y muestra los archivos con la extensión especificada, cambiando el nombre si contiene el texto a eliminar
     Get-ChildItem -Path $ruta -Filter "*.$extension" | ForEach-Object {
         $nombreAntes = $_.BaseName
         if ($nombreAntes -match $textoEliminar) {
@@ -81,22 +97,26 @@ function CambiarNombreArchivos {
     Write-Host "Se han renombrado $contadorArchivos archivos en total."
 }
 
+# Bucle principal para analizar y renombrar archivos
 do {
     $intentos++
     if ($intentos -eq 3) {
         Write-Host "`nDemasiados intentos fallidos. Saliendo del programa."
         exit
     }
+    # Solicita al usuario si desea analizar todos los archivos
     $analizarTodos = Read-Host "`nDeseas analizar todos los archivos de la carpeta? (s/n)"
     $extension = ValidarOpcion $analizarTodos
     ListarArchivos -ruta $rutaCarpeta -extension $extension
 } while ($extension -eq $null)
 
+# Bucle secundario para cambiar nombres y repetir la operación si se desea
 do {
     $textoEliminar = Read-Host "`nPor favor, ingresa el texto que deseas eliminar del nombre de los archivos"
     CambiarNombreArchivos -ruta $rutaCarpeta -extension $extension -textoEliminar $textoEliminar
     $repetir = Read-Host "`nDeseas realizar otra operacion? (s/n)"
     if ($repetir -eq "s") {
+        # Reinicia el contador de intentos y repite el proceso
         $intentos = 0
         do {
             $intentos++
@@ -113,5 +133,6 @@ do {
 
 Write-Host "`nFin del programa, gracias! :)"
 
+# Espera 10 segundos antes de limpiar la consola
 Start-Sleep -Seconds 10
 Clear-Host
